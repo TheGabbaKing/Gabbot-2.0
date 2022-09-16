4ee976ede60e2771301f3e62343c02745dbdb738
Gabba <thegabbaking@gmail.com>
2022-09-16 18:31:44 +1000

    Initialize Bot
    Creates bot with some basic commands

---
+++ cogs/commands.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import random
+
+class Commands(discord.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    ######### PREFIX COMMAND #########
+    @commands.command() #creates a prefixed commands
+    async def hello(self, ctx):
+        await ctx.send("Hello")
+    
+    ######### ON MESSAGE EVENT #########
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.content == "Aussie Aussie Aussie":
+            await ctx.channel.send("Oi Oi Oi")
+        if ctx.content == "miso":
+            await ctx.channel.send("horny")
+
+    ######### PENIS SIZE #########
+    @discord.slash_command(name="penis-calculator", description="Calculates your penis size")
+    async def penis_size(self, ctx):
+        sizes =[3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5, 5.25, 5.5, 5.75, 6, 6.25, 6.5, 6.75, 7, 7.25, 7.5, 7.75, 8]
+        penis = random.choice(sizes)
+        if penis < 5:
+            await ctx.respond(f"Your penis is {penis} inches long <:gyurikekpoint:1020239117960892416>") #change this to the ftb one
+        elif 5 <= penis < 6:
+            await ctx.respond(f"Your penis is {penis} inches long <a:aYunkyNotbad:792248677540167700>")
+        elif 6 <= penis < 7:
+            await ctx.respond(f"Your penis is {penis} inches long :hot_face:")
+        elif 7 <= penis < 8:
+            await ctx.respond(f"Your penis is {penis} inches long <a:COCKA:824761345509294152>")
+        else:
+            await ctx.respond(f"Your penis is {penis} inches long <:gigachad:948481454470484048>")
+            
+def setup(bot):
+    bot.add_cog(Commands(bot)) # add the cog to the bot
---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from dotenv import load_dotenv #dotenv is used for the .env file 
+from discord.ext import commands
+
+
+#### INITIALISATION ####
+load_dotenv() #loads all the varaiables in the .env file
+bot = commands.Bot(debug_guilds=[524776650945200138]) #debug_guilds serverID of test server (remove debug_guilds during production)
+bot = commands.Bot(intents=discord.Intents.all())
+bot.load_extension('cogs.commands')
+
+#### COMMANDS/EVENTS ####
+@bot.event 
+async def on_ready(): 
+    print(f"{bot.user} is ready and online!") #prints in the console that the bot is online
+
+@bot.slash_command(name="hello", description="say hello to the bot")
+async def say_hello(ctx):
+    await ctx.respond("Hey!") #directly replies to the user who used the slash command
+
+bot.run(os.getenv('TOKEN')) # run the bot with the token
+
+
+
